e2f06ab3a1f7a96dd325216da5b9c0259f8bb868
vineet-1437 <136501799+vineet-1437@users.noreply.github.com>
2025-02-21 02:12:08 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||HAPPY|BIRTHDAY|MY|CUTEE|OESHIKHA|❤|#rectangle|');
+      S.UI.simulate('|#countdown 3||HAPPY|BIRTHDAY|MY|CUTEE|OESHIKHA|❤|');
     }
 
     S.Drawing.loop(function () {
